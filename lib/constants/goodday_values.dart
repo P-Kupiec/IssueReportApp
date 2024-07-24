7f96652aa988516a2520d3ea6d146da53a63d629
Piotr Kupiec <piotr.kupiec@yahoo.com>
2024-07-24 22:28:11 +0200

    changed token

--- lib/constants/goodday_values.dart
+++ lib/constants/goodday_values.dart
@@ -2,6 +2,6 @@ class GoodDayValues {
   static const String post = "POST";
   static const String put = "PUT";
   static const String url = "https://api.goodday.work/2.0";
-  static const String token = "a15e14026c0541e39fe1c04587ca11bc";
+  static const String token = "sampleToken";
   static const String jsonType = "application/json";
 }
